5ea185e448a88d4b7377e5ea75f40b76203bddca
coolmanbat2 <thanusun.thayapara@mail.utoronto.ca>
2022-04-27 19:21:52 -0400

    Fixed button text and added animated background

---
+++ precache-manifest.c3663b989a9f295b05024fd0a8f26793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84778229f695161e26e02566729c1f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5276292bd3e0f27e63f8",
+    "url": "/static/css/main.cd897b66.chunk.css"
+  },
+  {
+    "revision": "c03671bbbb4085781a24",
+    "url": "/static/js/2.d07d6413.chunk.js"
+  },
+  {
+    "revision": "37c9336ebaf44a669d46",
+    "url": "/static/js/3.ef576390.chunk.js"
+  },
+  {
+    "revision": "5276292bd3e0f27e63f8",
+    "url": "/static/js/main.f0539efe.chunk.js"
+  },
+  {
+    "revision": "f030309dc27eea5ae05f",
+    "url": "/static/js/runtime~main.b17c8cf2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1ecbe1fe9026401d20ef94f6a0e4f1d.js"
+  "/precache-manifest.c3663b989a9f295b05024fd0a8f26793.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
